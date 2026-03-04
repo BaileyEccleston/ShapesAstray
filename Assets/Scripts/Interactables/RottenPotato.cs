3950a8df4e8250c51d06f0490ca2c1a512f851dc
BOLTON.AC.UK\be1crt <baileyeccleston0@gmail.com>
2026-03-04 15:26:22 +0000

    Final commit of Wednesday 4th

--- Assets/Scripts/Interactables/RottenPotato.cs
+++ Assets/Scripts/Interactables/RottenPotato.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 
 public class RottenPotato : MonoBehaviour
 {
-    bool coroutineRunning = false;
     private Coroutine coroutine = null;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,25 +13,21 @@ public class RottenPotato : MonoBehaviour
     public void Placed()
     {
         Debug.Log("Placed");
-        if (!coroutineRunning)
+        if (coroutine == null)
         {
             coroutine = StartCoroutine(RottenPotatoLife());
         }
         else
         {
-            if (coroutine != null)
-            {
-                StopCoroutine(coroutine);
-                coroutine = null;
-            }
+            StopCoroutine(coroutine);
+            coroutine = null;
         }
     }
 
     IEnumerator RottenPotatoLife()
     {
-        coroutineRunning = true;
+
         yield return new WaitForSeconds(15f);
-        coroutineRunning = false;
         Destroy(gameObject);
     }
 }
